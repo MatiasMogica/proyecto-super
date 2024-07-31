47bc0a6429a13f989ba4ff639748e69f4018b3cb
MatiasMogica <matimogica@gmail.com>
2024-07-31 11:10:30 -0300

    contacto validado

--- contacto/script.js
+++ contacto/script.js
@@ -1,28 +1,76 @@
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-let email = document.getElementById("email");
-let telefono = document.getElementById("telefono");
-let comentario = document.getElementById("comentario")
-let btnEnviar = document.getElementById("enviar");
-let informacion = [];
+let form = document.getElementById("formulario");
 
-btnEnviar.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    informacion[0] = nombre.value;
-    informacion[1] = apellido.value;
-    informacion[2] = email.value;
-    informacion[3] = telefono.value;
-    informacion[4] = comentario.value;
 
-    console.log(`Su nombre es ${informacion[0]} y su apellido es ${informacion[1]}, su email es: ${informacion[2]}, su telefono es: ${informacion[3]},
-            su comentario es: ${informacion[4]}`);
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let telefono = document.getElementById("telefono");
+    let comentario = document.getElementById("comentario");
+    let textoNombre = document.getElementById("nombreError");
+    let textoApellido = document.getElementById("apellidoError");
+    let textoEmail = document.getElementById("emailError");
+    let textoTelefono = document.getElementById("telefonoError");
+    let textoComentario = document.getElementById("comentarioError");
+    let informacion = [];
 
-    console.log(btnEnviar.className)
+    textoNombre.textContent = "";
+    textoApellido.textContent = "";
+    textoEmail.textContent = "";
+    textoTelefono.textContent = "";
+    textoComentario.textContent = "";
 
-    let blob = new Blob([informacion], { type: "text/plain;charset=utf-8" });
+    let emailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let telefonoValidate = /^\d{10}$/;
 
-    saveAs(blob, "contact.txt");
+    let valid = true;
 
-    console.log(blob)
-})
+    if (nombre.value === "") {
+        //alert("El nombre es obligatorio");
+        textoNombre.textContent = "El nombre es obligatorio";
+        valid = false;
+    } 
+    if (apellido.value === "") {
+        //alert("El apellido es obligatorio");
+        textoApellido.textContent = "El apellido es obligatorio";
+        console.log(textoApellido.textContent)
+        valid = false;
+    } 
+    if (!emailValidate.test(email.value)) {
+        //alert("Ingrese un correo válido");
+        textoEmail.textContent = "Ingrese un correo válido";
+        valid = false;
+    } 
+    if (!telefonoValidate.test(telefono.value)) {
+        //alert("Por favor, ingrese un número de teléfono válido de 10 dígitos.");
+        textoTelefono.textContent = "Por favor, ingrese un número de teléfono válido de 10 dígitos.";
+        valid = false;
+    } 
+    if (comentario.value === "") {
+        //alert("Por favor escriba un comentario");
+        textoComentario.textContent = "Por favor, escriba un comentario";
+        valid = false;
+    }
+
+    if (valid) {
+        informacion.push(`Nombre: ${nombre.value}`);
+        informacion.push(`Apellido: ${apellido.value}`);
+        informacion.push(`Email: ${email.value}`);
+        informacion.push(`Teléfono: ${telefono.value}`);
+        informacion.push(`Comentario: ${comentario.value}`);
+
+        let blob = new Blob([informacion.join('\n')], { type: "text/plain;charset=utf-8" });
+
+        saveAs(blob, "contact.txt");
+
+        nombre.value = "";
+        apellido.value = "";
+        email.value = "";
+        telefono.value = "";
+        comentario.value = "";
+
+        console.log(blob);
+    }
+});
 
